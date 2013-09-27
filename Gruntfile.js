ddd73817b482484c67b9ff09b2d944105636dc75
luto <m@luto.at>
2013-09-27 16:17:26 +0200

    Added grunt deps task to download deps

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,21 @@
 module.exports = function(grunt) {
 
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json')
+    pkg: grunt.file.readJSON('package.json'),
+    'curl-dir': {
+      'src/lib/js': [
+        'http://code.jquery.com/jquery-1.10.2.min.js',
+        'http://code.jquery.com/qunit/qunit-1.12.0.js',
+        'https://raw.github.com/AStepaniuk/qunit-parameterize/master/qunit-parameterize.js'
+      ],
+      'src/lib/css': [
+        'http://code.jquery.com/qunit/qunit-1.12.0.css'
+      ],
+    },
   });
 
-  grunt.registerTask('default', []);
+  grunt.loadNpmTasks('grunt-curl');
+
+  grunt.registerTask('default', [ ]);
+  grunt.registerTask('deps', [ 'curl-dir' ]);
 };
