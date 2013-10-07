14ed4897af378ba79da0657a48d9c953fda9db1f
fish <robert@programming-with-design.at>
2013-10-07 10:25:52 +0200

    Added: PWM

--- src/app/js/hardware.js
+++ src/app/js/hardware.js
@@ -59,8 +59,6 @@ if (typeof yasp == 'undefined') yasp = { };
      */
     LED: {
       render: function() {
-        var states = yasp.HardwareType.LED.States;
-        
         if (!this.element) {
           this.element = $('<canvas></canvas>');
           this.element.css({
@@ -78,10 +76,33 @@ if (typeof yasp == 'undefined') yasp = { };
           "height": height
         });
         
-        var radius = Math.min(width, height)/2;
         
+
+        if (!this.lighter && this.state == yasp.HardwareType.LED.States.OFF) {
+          this.dim = 5;
+          this.lighter = null;
+        } else if (!this.dim && this.state == yasp.HardwareType.LED.States.ON) {
+          this.dim = null;
+          this.lighter = 5;
+        } else {
+          this.dim = null;
+          this.lighter = null;
+        }
+        
+        // requestAnimFrame(this.type.animRender.bind(this));
+        this.type.animRender.call(this);
+      },
+      animRender: function() {
+        var states = yasp.HardwareType.LED.States;
+        
+        var width = this.element.width();
+        var height = this.element.height();
+        
+        var radius = Math.min(width, height)/2;
+
         var obj = this.element.get(0);
         var ctx = obj.getContext('2d');
+        
         ctx.clearRect(0,0,width, height); // clear it, baby
         
         // draw outer circle
@@ -89,25 +110,53 @@ if (typeof yasp == 'undefined') yasp = { };
         ctx.arc(width/2, height/2, radius, 0, 2 * Math.PI, false);
         ctx.fillStyle = 'rgba(10,10,10,0.9)';
         ctx.fill();
-        
+
         // draw main color
         ctx.beginPath();
         ctx.arc(width/2, height/2, radius-1, 0, 2 * Math.PI, false);
         ctx.fillStyle = this.state == states.ON ? this.params.onColor : this.params.offColor; // TODO: PWM
         ctx.fill();
         
+        // draw dim color
+        if (!!this.dim) {
+          ctx.globalAlpha = this.dim/5;
+          ctx.beginPath();
+          ctx.arc(width/2, height/2, radius-1, 0, 2 * Math.PI, false);
+          ctx.fillStyle = this.params.onColor; // TODO: PWM
+          ctx.fill();
+          ctx.globalAlpha = 1;
+        }
+
+        // draw lighter color
+        if (!!this.lighter) {
+          ctx.globalAlpha = this.lighter/5;
+          ctx.beginPath();
+          ctx.arc(width/2, height/2, radius-1, 0, 2 * Math.PI, false);
+          ctx.fillStyle = this.params.offColor; // TODO: PWM
+          ctx.fill();
+          ctx.globalAlpha = 1;
+        }
+        
         // draw inner circle
         ctx.beginPath();
         ctx.arc(width/2, height/2, radius-4, 0, 2 * Math.PI, false);
         ctx.strokeStyle = 'rgba(20,20,2,0.5)';
         ctx.stroke();
-        
+
         // draw white dot
         ctx.beginPath();
         ctx.arc(width/2 + 3, height/2 + 3, radius/6, 0, 2 * Math.PI, false);
         ctx.fillStyle = 'rgba(255,255,255,0.5)';
         ctx.fill();
-       
+        
+        if (!!this.dim) {
+          this.dim-=.5;
+          requestAnimFrame(this.type.animRender.bind(this));
+        }
+        if (!!this.lighter) {
+          this.lighter-=.5;
+          requestAnimFrame(this.type.animRender.bind(this));
+        }
       },
       initialState: function() {
         return yasp.HardwareType.LED.States.OFF;
@@ -169,7 +218,8 @@ if (typeof yasp == 'undefined') yasp = { };
 
   /**
    * Is internally callen to draw this hardware into the DOM
-   * It is not defined which method the hardware uses to draw: Some use Canvas, others manipulate the DOM directly
+   * It is not defined which method the hardware uses to draw: Some use Canvas, others manipulate the DOM directly.
+   * Do not call this on your own - only receiveStateChange may use this.
    */
   yasp.Hardware.prototype.render = function() {
     if (!!this.container) this.type.render.call(this);
--- src/app/js/utils.js
+++ src/app/js/utils.js
@@ -25,3 +25,10 @@ if (!Function.prototype.bind) {
     return fBound;
   };
 }
+
+window.requestAnimFrame = (function(callback) {
+  return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame ||
+    function(callback) {
+      window.setTimeout(callback, 1000 / 60);
+    };
+})();
