86e38e4a56a381b0db4e5326d850359325ba2dd3
luto <m@luto.at>
2013-11-11 16:26:47 +0100

    way simpler test-definitions

--- src/app/test/emulator/commands.js
+++ src/app/test/emulator/commands.js
@@ -1,243 +1,320 @@
 (function () {
   var emulator;
+  var assembler = new yasp.Assembler();;
 
   module("emulator commands", {
     setup: function () {
       emulator = new yasp.Emulator();
       emulator.stepping = true;
+      assembler = new yasp.Assembler();
     },
     teardown: function () {
       emulator = null;
+      assembler = null;
     }
   });
 
-  var commandTests = [
-    {
-      title: "MOV b0,1",
-      bitcode: new Uint8Array([0x00, 0x00, 0x01]),
-      ram: new Uint8Array([0x00, 0x00, 0x01]),
-      steps: [ { ram: { 0: 1 } } ]
-    },
-    {
-      title: "MOV b1,b0",
-      bitcode: new Uint8Array([0x10, 0x00, 0x20]),
-      ram: new Uint8Array([0x01, 0x00]),
-      steps: [ { ram: { 1: 1 } } ]
-    },
-    {
-      title: "MOV w0,0xFFAA",
-      bitcode: new Uint8Array([0x20, 0x00, 0xff, 0xaa]),
-      steps: [ { ram: { 0: 0xFF, 1: 0xAA } } ]
-    },
-    {
-      title: "MOV w1,w0",
-      bitcode: new Uint8Array([0x10, 0x40, 0x20]),
-      ram: new Uint8Array([0xBB, 0xAA, 0x00, 0x00]),
-      steps: [ { ram: { 2: 0xBB, 3: 0xAA } } ]
-    },
-    {
-      title: "ADD b0,b1",
-      bitcode: new Uint8Array([0x10, 0x04, 0x01]),
-      ram: new Uint8Array([0x01, 0x01]),
-      steps: [ { ram: { 0: 0x02 }, flags: { c: false, z: false } } ]
-    },
-    {
-      title: "ADD b0,b1 - carry",
-      bitcode: new Uint8Array([0x10, 0x04, 0x01]),
-      ram: new Uint8Array([0xFF, 0x02]),
-      steps: [ { ram: { 0: 0x01 }, flags: { c: true, z: false } } ]
-    },
-    {
-      title: "ADD b0,b1 - carry & zero",
-      bitcode: new Uint8Array([0x10, 0x04, 0x01]),
-      ram: new Uint8Array([0xFF, 0x01]),
-      steps: [ { ram: { 0: 0x00 }, flags: { c: true, z: true } } ]
-    },
-    {
-      title: "ADD b0,1",
-      bitcode: new Uint8Array([0x00, 0x20, 0x01]),
-      ram: new Uint8Array([0x01]),
-      steps: [ { ram: { 0: 0x02 }, flags: { c: false, z: false } } ]
-    },
-    {
-      title: "ADD b0,2 - carry",
-      bitcode: new Uint8Array([0x00, 0x20, 0x02]),
-      ram: new Uint8Array([0xFF]),
-      steps: [ { ram: { 0: 0x01 }, flags: { c: true, z: false } } ]
-    },
-    {
-      title: "ADD b0,1 - carry & zero",
-      bitcode: new Uint8Array([0x00, 0x20, 0x01]),
-      ram: new Uint8Array([0xFF]),
-      steps: [ { ram: { 0: 0x00 }, flags: { c: true, z: true } } ]
-    },
-    {
-      title: "ADD w0,1",
-      bitcode: new Uint8Array([0x20, 0x20, 0x01, 0x10]),
-      ram: new Uint8Array([0x02, 0xFA]),
-      steps: [ { ram: { 0: 0x04, 1: 0x0A }, flags: { c: false, z: false } } ]
-    },
-    {
-      title: "ADD w0,1 - 2nd byte",
-      bitcode: new Uint8Array([0x20, 0x20, 0x00, 0x01]),
-      ram: new Uint8Array([0x00, 0x00]),
-      steps: [ { ram: { 1: 0x01 }, flags: { c: false, z: false } } ]
-    },
-    {
-      title: "ADD w0,1 - 1st byte",
-      bitcode: new Uint8Array([0x20, 0x20, 0x01, 0x00]),
-      ram: new Uint8Array([0x00, 0x00]),
-      steps: [ { ram: { 0: 0x01 }, flags: { c: false, z: false } } ]
-    },
-    {
-      title: "ADD w0,2 - carry",
-      bitcode: new Uint8Array([0x20, 0x20, 0x00, 0x02]),
-      ram: new Uint8Array([0xFF, 0xFF]),
-      steps: [ { ram: { 0: 0x00, 1: 0x01 }, flags: { c: true, z: false } } ]
-    },
-    {
-      title: "ADD w0,1 - carry & zero",
-      bitcode: new Uint8Array([0x20, 0x20, 0x00, 0x01]),
-      ram: new Uint8Array([0xFF, 0xFF]),
-      steps: [ { ram: { 0: 0x00, 1: 0x00 }, flags: { c: true, z: true } } ]
-    },
-    {
-      title: "ADD w0,w1 - 2nd byte",
-      bitcode: new Uint8Array([0x10, 0x44, 0x01]),
-      ram: new Uint8Array([0x00, 0x01, 0x00, 0x01]),
-      steps: [ { ram: { 1: 0x02 }, flags: { c: false, z: false } } ]
-    },
-    {
-      title: "ADD w0,w1 - 1st byte",
-      bitcode: new Uint8Array([0x10, 0x44, 0x01]),
-      ram: new Uint8Array([0x01, 0x00, 0x01, 0x00]),
-      steps: [ { ram: { 0: 0x02 }, flags: { c: false, z: false } } ]
-    },
-    {
-      title: "ADD w0,w1",
-      bitcode: new Uint8Array([0x10, 0x44, 0x01]),
-      ram: new Uint8Array([0x0A, 0x10, 0x01, 0xFF]),
-      steps: [ { ram: { 0: 0x0C, 1: 0x0F }, flags: { c: false, z: false } } ]
-    },
-    {
-      title: "ADD w0,w1 - carry",
-      bitcode: new Uint8Array([0x10, 0x44, 0x01]),
-      ram: new Uint8Array([0xFF, 0xFF, 0x00, 0x02]),
-      steps: [ { ram: { 0: 0x00, 1: 0x01 }, flags: { c: true, z: false } } ]
-    },
-    {
-      title: "ADD w0,w1 - carry & zero",
-      bitcode: new Uint8Array([0x10, 0x44, 0x01]),
-      ram: new Uint8Array([0xFF, 0xFF, 0x00, 0x01]),
-      steps: [ { ram: { 0: 0x00, 1: 0x00 }, flags: { c: true, z: true } } ]
-    },
-    {
-      title: "RR b0 ; b0 = 64 - zero not set",
-      bitcode: new Uint8Array([0x40, 0x80]),
-      ram: new Uint8Array([0x40]),
-      steps: [ { ram: { 0: 0x20 }, flags: { c: false, z: false } } ]
-    },
-    {
-      title: "RR b0 ; b0 = 66 - zero not set",
-      bitcode: new Uint8Array([0x40, 0x80]),
-      ram: new Uint8Array([0x42]),
-      steps: [ { ram: { 0: 0x21 }, flags: { c: false, z: false } } ]
-    },
-    {
-      title: "RR w0 ; w0 = 0xFFAA - zero not set",
-      bitcode: new Uint8Array([0x60, 0x80]),
-      ram: new Uint8Array([0xFF, 0xAA]),
-      steps: [ { ram: { 0: 0x7F, 1: 0xD5 }, flags: { c: false, z: false } } ]
-    },
-    {
-      title: "RR w0 ; b0 = 0x1FF8 - zero not set",
-      bitcode: new Uint8Array([0x60, 0x80]),
-      ram: new Uint8Array([0x1F, 0xF8]),
-      steps: [ { ram: { 0: 0x0F, 1: 0xFC }, flags: { c: false, z: false } } ]
-    },
-    {
-      title: "RL b0 ; b0 = 64 - zero not set",
-      bitcode: new Uint8Array([0x40, 0xA0]),
-      ram: new Uint8Array([0x40]),
-      steps: [ { ram: { 0: 0x80 }, flags: { c: false, z: false } } ]
-    },
-    {
-      title: "RL b0 ; b0 = 63 - zero not set",
-      bitcode: new Uint8Array([0x40, 0xA0]),
-      ram: new Uint8Array([0x3F]),
-      steps: [ { ram: { 0: 0x7E }, flags: { c: false, z: false } } ]
-    },
-    {
-      title: "RL w0 ; b0 = 0xFFAA - zero not set",
-      bitcode: new Uint8Array([0x60, 0xA0]),
-      ram: new Uint8Array([0xFF, 0xAA]),
-      steps: [ { ram: { 0: 0xFF, 1: 0x54 }, flags: { c: true, z: false } } ]
-    },
-    {
-      title: "RL w0 ; b0 = 0x7B10 - zero  not set",
-      bitcode: new Uint8Array([0x60, 0xA0]),
-      ram: new Uint8Array([0x7B, 0x10]),
-      steps: [ { ram: { 0: 0xF6, 1: 0x20 }, flags: { c: false, z: false } } ]
-    },
-    {
-      title: "INV b0 ; b0 = 1",
-      bitcode: new Uint8Array([0x40, 0x40]),
-      ram: new Uint8Array([0x01]),
-      steps: [ { ram: { 0: 0xFE }, flags: { c:false, z: false } } ]
-    },
-    {
-      title: "INV b0 ; b0 = 0xFF, zero set",
-      bitcode: new Uint8Array([0x40, 0x40]),
-      ram: new Uint8Array([0xFF]),
-      steps: [ { ram: { 0: 0x00 }, flags: { c:false, z: true } } ]
-    },
-    {
-      title: "INV w0 ; w0 = 1",
-      bitcode: new Uint8Array([0x60, 0x40]),
-      ram: new Uint8Array([0x00, 0x01]),
-      steps: [ { ram: { 0: 0xFF, 1: 0xFE }, flags: { c:false, z: false } } ]
-    },
-    {
-      title: "INV w0 ; b0 = 0xFFFF, zero set",
-      bitcode: new Uint8Array([0x60, 0x40]),
-      ram: new Uint8Array([0xFF, 0xFF]),
-      steps: [ { ram: { 0: 0x00, 1: 0x00 }, flags: { c:false, z: true } } ]
-    },
-  ];
+  /*
+    Test-Format:
+      {
+        cmd: "MOV b0,b1",
+        setup: { reg: { "b2": 1 } },
+        steps: [
+          { reg: { "w0": 1, "b1": "00000001" } },
+          { ram: { 0x42: 1 }, pin: { .. to be defined .. } },
+          { rom: { 0x00: 0 }, flag: { c: false, z: true } }
+        ]
+      }
 
-  QUnit.cases(commandTests).test("command", function (params) {
-    emulator.pc = 0;
-    emulator.load(params.bitcode, 0);
+    Minimal example:
+      {
+        cmd: "MOV b0,b2",
+        setup: { reg: { "b2": 1 } },
+        steps: { reg: { "b0": 1 } }
+      }
+  */
 
-    if(params.ram) {
-      emulator.ram = params.ram;
+  var commandTestData = [];
+
+  // MOV
+  commandTestData = commandTestData.concat([
+    {
+      cmd: "MOV b0,1",
+      steps: { reg: { "b0": 1 } }
+    },
+    {
+      cmd: "MOV b0,b1",
+      setup: { reg: { "b1": 1 } },
+      steps: { reg: { "b0": 1 } }
+    },
+    {
+      cmd: "MOV w0,0xFFAA",
+      steps: { reg: { "w0": 0xFFAA } }
+    },
+    {
+      cmd: "MOV w0,w1",
+      setup: { reg: { "w1": 0xFFAA } },
+      steps: { reg: { "w0": 0xFFAA } }
     }
+  ]);
+
+  // ADD b,b
+  commandTestData = commandTestData.concat([
+    {
+      cmd: "ADD b0,b1",
+      setup: { reg: { "b0": 1, "b1": 1 } },
+      steps: { reg: { "b0": 2 }, flags: { c: false, z: false } }
+    },
+    {
+      cmd: "ADD b0,b1",
+      setup: { reg: { "b0": 255, "b1": 2 } },
+      steps: { reg: { "b0": 1 }, flags: { c: true, z: false } }
+    },
+    {
+      cmd: "ADD b0,b1",
+      setup: { reg: { "b0": 0xFF, "b1": 1 } },
+      steps: { reg: { "b0": 0 }, flags: { c: true, z: true } }
+    }
+  ]);
+
+  // ADD b,l
+  commandTestData = commandTestData.concat([
+    {
+      cmd: "ADD b0,1",
+      setup: { reg: { "b0": 1 } },
+      steps: { reg: { "b0": 2 }, flags: { c: false, z: false } }
+    },
+    {
+      cmd: "ADD b0,2",
+      setup: { reg: { "b0": 0xFF } },
+      steps: { reg: { "b0": 1 }, flags: { c: true, z: false } }
+    },
+    {
+      cmd: "ADD b0,1",
+      setup: { reg: { "b0": 0xFF } },
+      steps: { reg: { "b0": 0 }, flags: { c: true, z: true } }
+    }
+  ]);
+
+  // ADD w,l
+  commandTestData = commandTestData.concat([
+    {
+      cmd: "ADD w0,1",
+      setup: { reg: { "w0": 0x02FA } },
+      steps: { reg: { "w0": 0x02FB }, flags: { c: false, z: false } }
+    },
+    {
+      cmd: "ADD w0,2",
+      setup: { reg: { "w0": 0xFFFF } },
+      steps: { reg: { "w0": 0x0001 }, flags: { c: true, z: false } }
+    },
+    {
+      cmd: "ADD w0,1",
+      setup: { reg: { "w0": 0xFFFF } },
+      steps: { reg: { "w0": 0x0000 }, flags: { c: true, z: true } }
+    }
+  ]);
+
+  // ADD w,w
+  commandTestData = commandTestData.concat([
+    {
+      cmd: "ADD w0,w1",
+      setup: { reg: { "w0": 0x0A10, "w1": 0x01FF } },
+      steps: { reg: { "w0": 0x0C0F }, flags: { c: false, z: false } }
+    },
+    {
+      cmd: "ADD w0,w1",
+      setup: { reg: { "w0": 0xFFFF, "w1": 0x0002 } },
+      steps: { reg: { "w0": 0x0001 }, flags: { c: true, z: false } }
+    },
+    {
+      cmd: "ADD w0,w1",
+      setup: { reg: { "w0": 0xFFFF, "w1": 0x0001 } },
+      steps: { reg: { "w0": 0x0000 }, flags: { c: true, z: true } }
+    }
+  ]);
+
+  // RR
+  commandTestData = commandTestData.concat([
+    {
+      cmd: "RR b0",
+      setup: { reg: { "b0": "01000000" } },
+      steps: { reg: { "b0": "00100000" }, flags: { c: false, z: false } }
+    },
+    {
+      cmd: "RR b0",
+      setup: { reg: { "b0": "01000001" } },
+      steps: { reg: { "b0": "00100000" }, flags: { c: true, z: false } }
+    },
+    {
+      cmd: "RR w0",
+      setup: { reg: { "w0": "00011111 11111000" } },
+      steps: { reg: { "w0": "00001111 11111100" }, flags: { c: false, z: false } }
+    },
+    {
+      cmd: "RR w0",
+      setup: { reg: { "w0": "11111111 10101011" } },
+      steps: { reg: { "w0": "01111111 11010101" }, flags: { c: true, z: false } }
+    }
+  ]);
+
+  // RL
+  commandTestData = commandTestData.concat([
+    {
+      cmd: "RL b0",
+      setup: { reg: { "b0": "01000000" } },
+      steps: { reg: { "b0": "10000000" }, flags: { c: false, z: false } }
+    },
+    {
+      cmd: "RL b0",
+      setup: { reg: { "b0": "10000000" } },
+      steps: { reg: { "b0": "00000000" }, flags: { c: true, z: false } }
+    },
+    {
+      cmd: "RL w0",
+      setup: { reg: { "w0": "01111111 10101010" } },
+      steps: { reg: { "w0": "11111111 01010100" }, flags: { c: false, z: false } }
+    },
+    {
+      cmd: "RL w0",
+      setup: { reg: { "w0": "11111111 10101010" } },
+      steps: { reg: { "w0": "11111111 01010100" }, flags: { c: true, z: false } }
+    }
+  ]);
+
+
+  // INV-Commands
+  commandTestData = commandTestData.concat([
+    {
+      cmd: "INV b0",
+      setup: { reg: { "b0": "00000001" } },
+      steps: { reg: { "b0": "11111110" }, flags: { c:false, z: false } }
+    },
+    {
+      cmd: "INV b0",
+      setup: { reg: { "b0": "11111111" } },
+      steps: { reg: { "b0": "00000000" }, flags: { c:false, z: true } }
+    },
+    {
+      cmd: "INV w0",
+      setup: { reg: { "w0": "00000000 00000001" } },
+      steps: { reg: { "w0": "11111111 11111110" }, flags: { c:false, z: false } }
+    },
+    {
+      cmd: "INV w0",
+      setup: { reg: { "w0": "11111111 11111111" } },
+      steps: { reg: { "w0": "00000000 00000000" }, flags: { c:false, z: true } }
+    }
+  ]);
+
+  for (var i = 0; i < commandTestData.length; i++) {
+    var test = commandTestData[i];
+
+    test.title = test.cmd;
+    test.asm = assembler.assemble({ code: test.cmd, jobs: ["bitcode"] });
+  }
+
+  QUnit.cases(commandTestData).test("command", function (params) {
+    if(params.asm.success === false)
+      return ok(false, "Assembler failed");
+
+    emulator.pc = 0;
+    emulator.load(params.asm.bitcode, 0);
+
+    var setup = params.setup;
+
+    if(setup) {
+      if(setup.reg) {
+        for (var r in setup.reg) {
+          var val = parseRegValue(setup.reg[r]);
+
+          if(r.length < 2)
+          {
+            ok(false, "Invalid test: setup-register " + r);
+            continue;
+          }
+
+          var n = +r.substr(1);
+
+          if(r.charAt(0) === "b")
+            emulator.writeByteRegister(n, val);
+          else if (r.charAt(0) === "w")
+            emulator.writeWordRegister(n, val);
+          else
+            ok(false, "Invalid test: setup-register " + r + " does not exist.");
+        }
+      }
+    }
+
+    params.steps = params.steps || [];
+
+    if(!(params.steps instanceof Array))
+      params.steps = [params.steps];
 
     for (var i = 0; i < params.steps.length; i++) {
       var step = params.steps[i];
 
       emulator.tick();
 
-      if(step.ram) {
-        var addrs = Object.keys(step.ram);
+      if(step.reg) {
+        for (var r in step.reg) {
+          var actual;
+          var expected = parseRegValue(step.reg[r]);
 
-        for (var j = 0; j < addrs.length; j++) {
-          var addr = addrs[j];
-          var expected = step.ram[addr];
-          var actual = emulator.ram[addr];
-          strictEqual(actual, expected, "byte " + addr + " is " + expected)
+          if(r.length < 2)
+          {
+            ok(false, "Invalid test: step-register " + r);
+            continue;
+          }
+
+          var n = +r.substr(1);
+
+          if(r.charAt(0) === "b")
+            actual = emulator.readByteRegister(n);
+          else if (r.charAt(0) === "w")
+            actual = emulator.readWordRegister(n);
+          else
+          {
+            ok(false, "Invalid test: step-register " + r + " does not exist.");
+            continue;
+          }
+
+          strictEqual(actual, expected, "register " + r + " is " + expected);
         }
       }
       if(step.flags) {
         var flags = emulator.readFlags();
+
         for (var flag in flags) {
+          var longname = "unknown";
+          if(flag == "c")
+            longname = "carry";
+          if(flag == "z")
+            longname = "zero";
+          var msg = longname + " flag is " + (step.flags[flag] === true ? "set" : "not set");
+
           if(typeof step.flags[flag] === undefined && flags[flag] === false ||
              typeof step.flags[flag] !== undefined && flags[flag] == step.flags[flag])
-            ok(true, flag + " flag is " + flags[flag]);
+            ok(true, msg);
           else
-            ok(false, flag + " flag is " + flags[flag]);
+            ok(false, msg);
         }
       }
+      if(step.ram) {
+        alert("Step-RAM-Checking is not yet implemented")
+      }
+      if(step.rom) {
+        alert("Step-RAM-Checking is not yet implemented")
+      }
+      if(step.pin) {
+        alert("Step-RAM-Checking is not yet implemented")
+      }
+    }
+
+    function parseRegValue (val) {
+      if(typeof val === "string")
+        return parseInt(val.replace(' ', ''), 2);
+      return val;
     }
   });
 
