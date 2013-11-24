6cb6a00551670e45a9405a8d8feb040e6dcedb25
luto <m@luto.at>
2013-11-24 13:56:08 +0100

    doc for SUB

--- src/app/instructions/SUB_bb.js
+++ src/app/instructions/SUB_bb.js
@@ -1,6 +1,21 @@
 {
   "name": "SUB",
-  "description": "",
+  "doc": {
+    "de": {
+      "description": "Subtrahiert den Wert des zweiten Registers von dem des ersten.",
+      "flags": {
+        "z": "wird gesetzt wenn das Ergebnis 0 ist",
+        "c": "wird gesetzt wenn das Ergebnis kleiner als 0 ist"
+      }
+    },
+    "en": {
+      "description": "Subtracts the value of the second register from the first one.",
+      "flags": {
+        "z": "is set if the result is 0",
+        "c": "is set if the result smaller than 0"
+      }
+    }
+  },
   "code": [
     {
       "value": 0x10
--- src/app/instructions/SUB_wl.js
+++ src/app/instructions/SUB_wl.js
@@ -1,6 +1,21 @@
 {
   "name": "SUB",
-  "description": "",
+  "doc": {
+    "de": {
+      "description": "Subtrahiert den gegebenen Wert von dem Wert des Registers.",
+      "flags": {
+        "z": "wird gesetzt wenn das Ergebnis 0 ist",
+        "c": "wird gesetzt wenn das Ergebnis kleiner als 0 ist"
+      }
+    },
+    "en": {
+      "description": "Subtracts the given value from the value of the first register.",
+      "flags": {
+        "z": "is set if the result is 0",
+        "c": "is set if the result smaller than 0"
+      }
+    }
+  },
   "code": [
     {
       "value": 0x20
