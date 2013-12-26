11bf49f43f23e3cc76e54c9feff4076a5190e570
luto <m@luto.at>
2013-12-26 08:31:42 +0100

    de locale

--- src/app/langs/de.js
+++ src/app/langs/de.js
@@ -4,6 +4,58 @@ if (yasp.l10n.lang === undefined) yasp.l10n.lang = { };
 
 (function() {
   yasp.l10n.lang.de = {
-    "about.title": "About"
+
+    "title": "yasp",
+
+    "editor.toolbar.run": "Startem",
+    "editor.toolbar.help": "Hilfe",
+    "editor.toolbar.menu": "Menü",
+
+    "editor.toolbar.menu.run": "Starten",
+    "editor.toolbar.menu.undo": "Undo",
+    "editor.toolbar.menu.redo": "Redo",
+    "editor.toolbar.menu.new": "Neu",
+    "editor.toolbar.menu.save": "Speichern",
+    "editor.toolbar.menu.saveAs": "Speichern als",
+    "editor.toolbar.menu.open": "Öffnen",
+    "editor.toolbar.menu.find": "Suchen",
+    "editor.toolbar.menu.replace": "Ersetzen",
+    "editor.toolbar.menu.goToLine": "Gehe zu zeile",
+    "editor.toolbar.menu.help": "Hilfe",
+    "editor.toolbar.menu.settings": "Einstellungen",
+    "editor.toolbar.menu.about": "About",
+
+    "editor.about.title": "About",
+    "editor.about.description":
+      "yasp is a fully functional web-based USB-Master development environment."       +
+        "It features an integrated development environment, binary compatible emulator," +
+        "cutting-edge Debugger and highly sophisticated assembler. The development suit" +
+        "aims to make learning assembler programming easy and accessible for everyone.",
+    "editor.about.version": "Version",
+    "editor.about.team": "Team",
+    "editor.about.specialthanks": "Besonderer Dank geht an",
+
+    "debugger.title": "Debugger",
+    "debugger.close": "Schließen",
+    "debugger.toolbar.step": "Step",
+    "debugger.toolbar.stepOver": "Step Over",
+    "debugger.toolbar.stepOut": "Step Out",
+    "debugger.toolbar.stepBack": "Step Back",
+    "debugger.toolbar.break": "Break",
+    "debugger.toolbar.continue": "Continue",
+
+    "help.title": "Hilfe",
+    "help.close": "Schließen",
+
+    "settings.title": "Einstellungen",
+    "settings.theme": "Theme",
+    "settings.tabSize": "Tab-Breite",
+    "settings.tabSize.spaces": "{0} leerzeichen",
+    "settings.language": "Sprache",
+    "settings.autoSave": "Automatisches Speichern",
+    "settings.codeCompletion": "Codevervollständigung",
+    "settings.showLabelList": "Label-Liste anzeigen",
+    "settings.showHelp": "Hilfe anzeigen",
+    "settings.close": "Schließen",
   };
 })();
