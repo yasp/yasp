68183055b7ab18ebefb73391ba6b05b1b2a73c82
luto <m@luto.at>
2014-01-03 21:14:18 +0100

    l10n: fix error when there is no lang set

--- src/app/js/localization.js
+++ src/app/js/localization.js
@@ -2,7 +2,7 @@ if (typeof yasp == 'undefined') yasp = { };
 
 (function() {
   yasp.l10n.getLangName = function () {
-    var name = yasp.Storage["language"].toLowerCase();
+    var name = (yasp.Storage["language"] || "").toLowerCase();
     if(!yasp.l10n.lang[name])
       name = "en";
     return name;
