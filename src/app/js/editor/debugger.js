5bc7907f1deee33ec1dfbc1b0735fe5c77269afb
luto <m@luto.at>
2014-01-17 14:44:26 +0100

    debugger: clear last executed line on exit

--- src/app/js/editor/debugger.js
+++ src/app/js/editor/debugger.js
@@ -138,6 +138,8 @@ if (typeof yasp == 'undefined') yasp = { };
         yasp.Debugger.EmulatorCommunicator = null;
       }
 
+      yasp.Debugger.editor.removeLineClass(yasp.Debugger.lastExecutedLine, 'background', 'line-active');
+
       if(yasp.Debugger.breadboard) {
         yasp.Debugger.breadboard.destroy();
       }
