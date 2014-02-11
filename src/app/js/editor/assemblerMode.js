52985d9e5405d04d8bb806c482f3f5d7c22031fb
fish <robert@programming-with-design.at>
2014-02-11 12:28:12 +0100

    Fixed: Comments are intended

--- src/app/js/editor/assemblerMode.js
+++ src/app/js/editor/assemblerMode.js
@@ -58,7 +58,7 @@
       indent: function(state, textAfter) {
         var tokens = !!state.tokens ? state.tokens : new yasp.Lexer().pass({ }, textAfter);
         
-        if (tokens.length == 1) return CodeMirror.Pass; // lexer always appends \n
+        if (tokens.length == 1) return indentUnit; // lexer always appends \n
         var t = tokens[0];
         if (t.text.toUpperCase() == "END") return 0;
         
